d3dc062a366956008e66cce9cac3c3cc18af6970
DanyilYatskov <68867876+DanyilYatskov@users.noreply.github.com>
2021-02-24 19:29:36 +0200

    added lightbox

--- src/js/start.js
+++ src/js/start.js
@@ -1,6 +1,8 @@
 import refs from './refs';
 import fetchAPI from './fetchAPI';
 import render from './renderGallery';
+import * as basicLightbox from 'basiclightbox';
+import '../styles/basicLightbox.min.css';
 function onFormSubmit(event) {
   event.preventDefault();
   const form = event.currentTarget;
@@ -13,15 +15,44 @@ refs.searchFormRef.addEventListener('submit', event => {
   refs.galleryRef.innerHTML = '';
   refs.moreImagesBtnRef.classList.add('is-hidden');
   fetchAPI.resetPageToFirst();
+  // fetchAPI.fetchImages().then(hits => {
+  //   if (hits.length == 0) {
+  //     return;
+  //   }
+  //   refs.moreImagesBtnRef.classList.remove('is-hidden');
+  //   return render(hits, refs.galleryRef);
+  // });
+  fetchImages();
+});
+refs.moreImagesBtnRef.addEventListener('click', moreImagesOnClick);
+function moreImagesOnClick() {
+  // fetchAPI.fetchImages().then(hits => render(hits, refs.galleryRef));
+  // window.scrollTo({
+  //   top: document.documentElement.offsetHeight,
+  //   behavior: 'smooth',
+  // });
+  fetchImages();
+}
+function fetchImages() {
   fetchAPI.fetchImages().then(hits => {
     if (hits.length == 0) {
       return;
     }
+    render(hits, refs.galleryRef);
     refs.moreImagesBtnRef.classList.remove('is-hidden');
-    return render(hits, refs.galleryRef);
+    window.scrollTo({
+      top: document.documentElement.offsetHeight,
+      behavior: 'smooth',
+    });
   });
-});
-refs.moreImagesBtnRef.addEventListener('click', moreImagesOnClick);
-function moreImagesOnClick() {
-  fetchAPI.fetchImages().then(hits => render(hits, refs.galleryRef));
 }
+function onImageClick(event) {
+  if (event.target.nodeName !== 'IMG') {
+    return;
+  }
+  const lightBox = basicLightbox.create(`
+        <img src="${event.target.dataset.source}" alt="" />
+      `);
+  lightBox.show();
+}
+refs.galleryRef.addEventListener('click', onImageClick);
